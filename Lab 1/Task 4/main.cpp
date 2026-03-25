79469d852142c4cc5fb2c9f7b25ace8afcbfc58f
OliverRysnik <oliver2.rysnik@live.uwe.ac.uk>
2026-03-25 14:39:00 +0000

    Create main.cpp

---
+++ Lab 1/Task 4/main.cpp
@@ -0,0 +1,29 @@
+#include "mbed.h"
+// Blinking rate in milliseconds
+#define BLINKING_RATE     200ms
+
+int main()
+{
+    // Initialise the digital pin LED1 as an output
+    DigitalOut led1(LED1);
+    DigitalOut led2(LED2);
+    DigitalOut led3(LED3);
+
+    for(int i = 0; i < 5; i++){
+        led1 = 1;
+        led2 = 1;
+        led3 = 1;
+        ThisThread::sleep_for(BLINKING_RATE);
+
+        led1 = 0;
+        led2 = 0;
+        led3 = 0;
+        ThisThread::sleep_for(BLINKING_RATE);
+    }
+
+    while (true) {
+        led1 = 1;
+        led2 = 0;
+        led3 = 0;
+    }
+}
