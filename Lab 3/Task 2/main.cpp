8caa1aab0b43dad932316860550970b5d5ce546e
OliverRysnik <oliver2.rysnik@live.uwe.ac.uk>
2026-04-09 16:50:26 +0100

    Update main.cpp

--- Lab 3/Task 2/main.cpp
+++ Lab 3/Task 2/main.cpp
@@ -1,1 +1,110 @@
+//=====[Libraries]=====
 
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+//=====[Declaration and initialization of public global objects]=====
+
+//Buttons for the board to simulate switches
+DigitalIn switch1(D2); //Simulates Gas Detection
+DigitalIn switch3(D3); //Simulates Over-Temperature Detection
+DigitalIn switch5(D6); //Reset Alarms
+DigitalOut alarmLed(LED1);
+UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
+
+//=====[Declaration and initialization of public global variables]=====
+
+bool gasAlarmState = OFF;
+bool tempAlarmState = OFF;
+
+//=====[Declarations (prototypes) of public functions]=====
+
+void inputsInit();
+void outputsInit();
+void alarmSystemUpdate();
+void uartTask();
+
+//=====[Main function]=====
+
+int main()
+{
+    inputsInit();
+    outputsInit();
+    
+    while (true) {
+        alarmSystemUpdate();
+        uartTask();
+    }
+}
+
+//=====[Implementations of public functions]=====
+
+void inputsInit()
+{
+    switch1.mode(PullDown);
+    switch3.mode(PullDown);
+    switch5.mode(PullDown);
+}
+
+void outputsInit()
+{
+    alarmLed = OFF;
+}
+
+void alarmSystemUpdate()
+{
+    //If Switch 1 is pressed trigger the gas alarm
+    if ( switch1 ) {
+        if (gasAlarmState == OFF) {
+            uartUsb.write( "WARNING: GAS DETECTED\r\n", 23 );
+        }
+        gasAlarmState = ON;
+    }
+
+    //If Switch 3 is pressed trigger the over-temp alarm
+    if ( switch3 ) {
+        if (tempAlarmState == OFF) {
+            uartUsb.write( "WARNING: TEMPERATURE TOO HIGH\r\n", 31 );
+        }
+        tempAlarmState = ON;
+    }
+
+    //If Switch 5 is pressed, reset the alarm
+    if ( switch5 ) {
+        if (gasAlarmState == ON || tempAlarmState == ON) { //Check both alarms
+            uartUsb.write( "ALARMS RESET\r\n", 14 );
+        }
+        gasAlarmState = OFF;
+        tempAlarmState = OFF; //Reset temp alarm
+    }
+
+    //LED is ON if either alarm is active
+    alarmLed = gasAlarmState || tempAlarmState; //Logic check
+}
+
+void uartTask()
+{
+    char receivedChar = '\0';
+    
+    if( uartUsb.readable() ) {
+        uartUsb.read( &receivedChar, 1 );
+        
+        //If 2 is pressed on keyboard, send Gas Alarm state
+        if ( receivedChar == '2' ) {
+            if ( gasAlarmState ) {
+                uartUsb.write( "GAS ALARM ACTIVE\r\n", 18 );
+            } else {
+                uartUsb.write( "GAS ALARM CLEAR\r\n", 17 );
+            }
+        }
+
+        //If 3 is pressed on keyboard, send Temp Alarm state
+        if ( receivedChar == '3' ) {
+            if ( tempAlarmState ) {
+                uartUsb.write( "TEMP ALARM ACTIVE\r\n", 19 );
+            } else {
+                uartUsb.write( "TEMP ALARM CLEAR\r\n", 18 );
+            }
+        }
+    }
+}
