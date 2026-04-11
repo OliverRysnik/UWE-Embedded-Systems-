64e1a61f45800de8e2743f8cedce765d78cbe62f
OliverRysnik <oliver2.rysnik@live.uwe.ac.uk>
2026-04-11 21:01:18 +0100

    Update main.cpp

--- Lab 1/Task 2/main.cpp
+++ Lab 1/Task 2/main.cpp
@@ -1,12 +1,15 @@
 #include "mbed.h"
-// Blinking rate in milliseconds
+
+//Blink rate in milliseconds
 #define BLINKING_RATE     500ms
+
 int main()
 {
-    // Initialise the digital pin LED1 as an output
+    //Initialise the digital pin LED1 as an output
     DigitalOut led1(LED1);
     DigitalOut led2(LED2);
     DigitalOut led3(LED3);
+    
     //keep track of the rate
     int rate = 0;
 
@@ -14,18 +17,20 @@ int main()
         //led2 blinks at 500ms so keep it the same
         led2 = !led2;
         
-        //led1 blinks every 1 sec so double the rate
+        //led1 blinks every 1 sec, so double the rate
         if (rate % 2 == 0){
             led1 = !led1;
         }
-        //led3 blinks every 2 sec so quad the rate 
+        //led3 blinks every 2 sec, so quad the rate 
         if (rate % 4 == 0){
             led3 = !led3;
         }
 
         ThisThread::sleep_for(BLINKING_RATE);
+        
         //Adds 1 to the counter
         rate++; 
+        
         //makes the rate loop repeat after 4 counts have passed
         if (rate >= 4){
             rate = 0;
