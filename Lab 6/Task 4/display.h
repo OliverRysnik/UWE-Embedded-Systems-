c30d1e45c68271d072b70dc7776994c844edf212
OliverRysnik <oliver2.rysnik@live.uwe.ac.uk>
2026-04-17 18:36:24 +0100

    Create display.h

---
+++ Lab 6/Task 4/display.h
@@ -0,0 +1,20 @@
+#ifndef _DISPLAY_H_
+#define _DISPLAY_H_
+
+typedef enum {
+     DISPLAY_CONNECTION_GPIO_4BITS,
+     DISPLAY_CONNECTION_GPIO_8BITS,
+     DISPLAY_CONNECTION_I2C_PCF8574_IO_EXPANDER,
+} displayConnection_t;
+
+typedef struct {
+   displayConnection_t connection;                                                 
+} display_t;
+
+void displayInit( displayConnection_t connection );
+ 
+void displayCharPositionWrite( uint8_t charPositionX, uint8_t charPositionY );
+
+void displayStringWrite( const char * str );
+
+#endif
